d19cb778df85e2b49b023af4a4f4b2e388ba73a6
jinwook song <wlsdnr129@naver.com>
2025-03-31 23:42:25 +0900

    feat: layout

---
+++ tutorial/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<header>Landing Page</header>
+
+{@render children()}
---
+++ tutorial/src/routes/blog/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<header>Header</header>
+
+{@render children()}
