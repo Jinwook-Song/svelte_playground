e91005af55baa320e91353af0cca697b3b8b4ef4
jinwook song <wlsdnr129@naver.com>
2025-03-31 20:49:59 +0900

    feat(rune): state

--- tutorial/src/routes/+page.svelte
+++ tutorial/src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	let count = $state(0);
+	let userInfo = $derived(count === 0 ? '초기값' : `${count} 값이 있습니다.`);
+
+	function increment() {
+		count += 1;
+	}
+</script>
+
+<div>
+	<p>Count: {count}</p>
+	<button onclick={increment}>증가</button>
+	<p>User Info: {userInfo}</p>
+</div>
