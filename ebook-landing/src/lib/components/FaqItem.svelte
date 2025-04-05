b724853db05791f0076c29d77f2a355c004b33d6
jinwook song <wlsdnr129@naver.com>
2025-04-05 21:12:45 +0900

    feat: faq

--- ebook-landing/src/lib/components/FaqItem.svelte
+++ ebook-landing/src/lib/components/FaqItem.svelte
@@ -1,3 +1,19 @@
+<script>
+  import chevronDown from '$assets/icons/chevron-down.svg';
+
+  let { faq, isExpanded, ...props } = $props();
+</script>
+
+<button class="container" class:container-expanded={isExpanded} {...props}>
+  <div class="question-and-answer">
+    <p class="question mb-s">{faq.question}</p>
+    <p class="answer">
+      {faq.answer}
+    </p>
+  </div>
+  <img src={chevronDown} alt="" />
+</button>
+
 <style>
   .container {
     display: flex;
@@ -25,7 +41,7 @@
   button img {
     width: 24px;
     height: 24px;
-    transition: transform 0.3s linear;
+    transition: transform 150ms linear;
   }
 
   .question {
